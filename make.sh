4fdb5b1403d987d639e2eeec390ecee3d917d90f
Julian <81520713+Juliaaan2502@users.noreply.github.com>
2021-06-12 11:58:26 +0200

    Add files via upload

--- make.sh
+++ make.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-if [[ $1 = "clean" ]]; then
-echo "Removing Build-Dir..."
-rm -r build
-
-else
 mkdir build
+
 nasm -f elf64 header.asm -o build/header.o
 nasm -f elf64 kernel.asm -o build/kernel.o
-ld --nmagic --output=build/kernel --script=linker.ld build/header.o build/kernel.o
 
-fi
+ld --nmagic --output=build/kernel --script=linker.ld build/header.o build/kernel.o
